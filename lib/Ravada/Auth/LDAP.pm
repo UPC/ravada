cd95eb70bd35a8a41145661b71b913a46069fc8c
Francesc Guasch <frankie@etsetb.upc.edu>
2016-04-25 15:57:28 +0200

    get LDAP dc base from the domain name

--- lib/Ravada/Auth/LDAP.pm
+++ lib/Ravada/Auth/LDAP.pm
@@ -4,21 +4,24 @@ use strict;
 use warnings;
 
 use Net::LDAP;
+use Net::Domain qw(hostdomain);
 
 our $LDAP;
+our $BASE;
 
 sub login {
     my ($username, $password) = @_;
 
     _init_ldap();
 
+    my $base = _dc_base();
     my $search = $LDAP->search(      # Search for the user
-    base   => 'DC=casa,DC=guru',
+    base   => $base,
     scope  => 'sub',
     filter => "(&(uid=$username))",
     attrs  => ['dn']
     );
-    die "not found" if not $search->count;
+    die "not found '$username' at '$base'" if not $search->count;
 
     my $user_dn = $search->entry->dn;
 
@@ -29,6 +32,17 @@ sub login {
 
     warn "ERROR: ".$mesg->code." : ".$mesg->error. " : Bad credentials for $username";
     return;
+}
+
+sub _dc_base {
+    # TODO: from config
+
+    my $base = '';
+    for my $part (split /\./,hostdomain()) {
+        $base .= "," if $base;
+        $base .= "dc=$part";
+    }
+    return $base;
 }
 
 sub _init_ldap {
