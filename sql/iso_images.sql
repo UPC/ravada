1a9034004e6447b596a030754354e150d098f32d
Francesc Guasch <frankie@etsetb.upc.edu>
2016-04-29 16:49:31 +0200

    first steps for base images creation

--- sql/iso_images.sql
+++ sql/iso_images.sql
@@ -2,17 +2,24 @@ CREATE TABLE `iso_images` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` char(64) NOT NULL,
   `description` varchar(255),
+  `arch` char(8),
+  `xml`  varchar(64),
+  `xml_volume` varchar(64),
   `url` varchar(255),
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
 );
 
 INSERT INTO iso_images
-(name,description,url)
+(name,description,arch,xml,xml_volume,url)
 VALUES('Ubuntu Xenial Xerus 32 bits','Ubuntu 16.04 LTS Xenial Xerus 32 bits'
+    ,'i386'
+    ,'xenial-i386.xml'
+    ,'xenial-volume.xml'
     ,'http://releases.ubuntu.com/16.04/ubuntu-16.04-desktop-i386.iso');
 
 INSERT INTO iso_images
-(name,description,url)
+(name,description,arch,url)
 VALUES('Ubuntu Xenial Xerus 64 bits','Ubuntu 16.04 LTS Xenial Xerus 64 bits'
+        ,'amd64'
         ,'http://releases.ubuntu.com/16.04/ubuntu-16.04-desktop-amd64.iso');
