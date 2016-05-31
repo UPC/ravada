e3847ec382b5640f39ebb19fbcf71e447186f7b1
joansp <joan.sanchezprats@gmail.com>
2016-05-31 15:17:06 +0200

    fixed deep recursion

--- lib/Ravada.pm
+++ lib/Ravada.pm
@@ -92,10 +92,7 @@ sub _init_config {
 
 sub _create_vm {
     my $self = shift;
-    my $vm_kvm;
-    eval { $vm_kvm = Ravada::VM::KVM->new( connector => ( $self->connector or $CONNECTOR ))  };
-
-    $self->vm->[0] = $vm_kvm if $vm_kvm;
+    return [ Ravada::VM::KVM->new( connector => ( $self->connector or $CONNECTOR )) ];
 }
 
 =head2 create_domain
