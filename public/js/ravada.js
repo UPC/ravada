eba406cc61bdb6cbd8d2ba6ee50c5ffa099bef60
joansp <joan.sanchezprats@gmail.com>
2016-06-01 18:58:14 +0200

    Angular.js Support Contact Form

--- public/js/ravada.js
+++ public/js/ravada.js
@@ -1,15 +1,32 @@
-angular.module("ravada.app",[])
+var app = angular.module("ravada.app",[]);
  
-.controller("new_base",[ '$scope', '$http', function($scope, $http) {
-    $http.get('/list_images.json').then(function(response) {
-            $scope.images = response.data;
-    });
-    
-}])
+    app.controller("new_base",[ '$scope', '$http', function($scope, $http) {
 
-.directive("solShowNewbase",function() {
-  return {
-    restrict: "E",
-    templateUrl: '/templates/new_base.html',
-  };
-})
+        $http.get('/list_images.json').then(function(response) {
+                $scope.images = response.data;
+        });
+
+    }]);
+
+    app.controller("SupportForm",function($scope){
+        $scope.showErr = false;
+        $scope.isOkey = function() {
+            if($scope.contactForm.$valid){
+                $scope.showErr = false;
+            } else{
+                $scope.showErr = true;
+            }
+        }
+
+    });
+
+    app.directive("solShowNewbase",function() {
+
+        return {
+            restrict: "E",
+            templateUrl: '/templates/new_base.html',
+        };
+
+    });
+
+
