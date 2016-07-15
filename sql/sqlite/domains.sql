8d5eac91c96d72c673ffdba18aff1c59b9705054
Francesc Guasch <frankie@etsetb.upc.edu>
2016-07-15 14:00:13 +0200

    boolean fields now are 1 and 0
    So it is easier to access and use them from Perl

--- sql/sqlite/domains.sql
+++ sql/sqlite/domains.sql
@@ -2,10 +2,10 @@ CREATE TABLE `domains` (
   `id` integer primary key AUTOINCREMENT,
   `id_base` int(11) ,
   `name` char(80) NOT NULL,
-  `created` char(1) NOT NULL DEFAULT 'n',
+  `created` char(1) NOT NULL DEFAULT '0',
   `error` varchar(200) DEFAULT NULL,
   `uri` varchar(250) DEFAULT NULL,
-  `is_base` char(1) NOT NULL DEFAULT 'n',
+  `is_base` char(1) NOT NULL DEFAULT '0',
   `file_base_img` varchar(255) DEFAULT NULL,
   UNIQUE (`id_base`,`name`),
   UNIQUE (`name`)
