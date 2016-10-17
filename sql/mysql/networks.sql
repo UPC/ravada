5545a64d4b2905ee751b3095c613f614d59a3909
Francesc Guasch <frankie@etsetb.upc.edu>
2016-10-17 10:07:11 +0200

    changed ip by address and added a no domains field

--- sql/mysql/networks.sql
+++ sql/mysql/networks.sql
@@ -1,8 +1,9 @@
 CREATE TABLE `networks` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(32) NOT NULL,
-  `ip` varchar(32) NOT NULL,
+  `address` varchar(32) NOT NULL,
   `description` varchar(140) DEFAULT NULL,
   `all_domains` int(11) DEFAULT '0',
+  `no_domains` int(11) DEFAULT '0',
   PRIMARY KEY (`id`)
 );
--- sql/sqlite/networks.sql
+++ sql/sqlite/networks.sql
@@ -1,7 +1,8 @@
 CREATE TABLE `networks` (
   `id` integer primary key AUTOINCREMENT,
   `name` varchar(32) NOT NULL,
-  `ip` varchar(32) NOT NULL,
+  `address` varchar(32) NOT NULL,
   `description` varchar(140) DEFAULT NULL,
   `all_domains` int(11) DEFAULT '0'
+  `no_domains` int(11) DEFAULT '0'
 );
