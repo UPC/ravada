e98e508ac94f640c0d76e6f7bc7fc054de034179
Francesc Guasch <frankie@etsetb.upc.edu>
2016-10-27 09:30:21 +0200

    code doc for LDAP support

--- lib/Ravada/Auth.pm
+++ lib/Ravada/Auth.pm
@@ -55,6 +55,16 @@ sub login {
     return Ravada::Auth::SQL->new(name => $name, password => $pass);
 }
 
+=head2 LDAP
+
+Sets or get LDAP support.
+
+    Ravada::Auth::LDAP(0);
+
+    print "LDAP is supported" if Ravada::Auth::LDAP();
+
+=cut
+
 sub LDAP {
     my $value = shift;
     return $LDAP if !defined $value;
