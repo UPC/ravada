3728dde05ebf99862a99c3691531983b97163bb0
Francesc Guasch <frankie@etsetb.upc.edu>
2017-02-27 09:48:13 +0100

    [#127] xml path no needed

--- sql/mysql/domain_settings_types.sql
+++ sql/mysql/domain_settings_types.sql
@@ -2,7 +2,6 @@ CREATE TABLE `domain_settings_types` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` char(32) DEFAULT NULL,
   `description` varchar(200) DEFAULT NULL,
-  `xml_path` varchar(200) DEFAULT NULL,
   `vm` char(32),
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
