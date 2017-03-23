608aa67dc5cc23a2b3c7a1e4502de7bec4d951ff
Francesc Guasch <frankie@etsetb.upc.edu>
2017-03-23 18:32:48 +0100

    [#146] Store target device for each file base

--- sql/mysql/file_base_images.sql
+++ sql/mysql/file_base_images.sql
@@ -2,5 +2,6 @@ CREATE TABLE `file_base_images` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_domain` int(11),
   `file_base_img` varchar(255) DEFAULT NULL,
+  `target` varchar(64) DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
