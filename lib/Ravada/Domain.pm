951faa3f396dc3a83ff8e47097036709310b487b
Francesc Guasch <frankie@etsetb.upc.edu>
2017-04-12 12:17:35 +0200

    Fixed develop merge for pre remove domain

--- lib/Ravada/Domain.pm
+++ lib/Ravada/Domain.pm
@@ -496,7 +496,7 @@ sub _pre_remove_domain {
     eval { $self->id };
     $self->pre_remove();
     $self->_allow_remove(@_);
-    $self->pre_remove_domain();
+    $self->pre_remove();
 }
 
 sub _after_remove_domain {
