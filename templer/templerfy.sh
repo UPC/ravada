ddb4f2eb32373b97c9eaf2bed3941e4d2d11839a
fv3rdugo <fv3rdugo@gmail.com>
2017-04-28 20:53:04 +0200

    Add bash script to templer generation

---
+++ templer/templerfy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#TODO
+# pod2html.pl
+PATH1="/src/ravada"
+
+echo "change repo directory..."
+eval cd ~$PATH1
+git checkout master && echo "Checkout master..."|| echo "Checkout master failed!"
+eval mkdir -p ~$PATH1/documentation/docs ~$PATH1/documentation/devel-docs
+mkdir -p /tmp/mds/docs /tmp/mds/devel-docs
+cp docs/* /tmp/mds/docs
+cp devel-docs/* /tmp/mds/devel-docs
+git checkout gh-pages && echo "Checkout gh-pages..."|| echo "Checkout gh-pages failed!"
+for i in `ls /tmp/mds/docs/*.md`;do
+    echo -e "title: $(basename $i .md)\n----\n$(cat $i)" > $i
+    eval cp $i ~$PATH1/templer/input/docs/
+done
+
+for i in `ls /tmp/mds/devel-docs/*.md`;do
+    echo -e "title: $(basename $i .md)\n----\n$(cat $i)" > $i
+    eval cp $i ~$PATH1/templer/input/devel-docs/
+done
+
+#Run templer and generate the output in 
+eval cd ~$PATH1/templer
+echo "Generation static pages..."
+templer
+echo "Deleting input directory..."
+echo "Remember upload to git, with git push"
+rm -rf input/docs/* input/devel-docs/* 
+
+#Delete al temp files
+rm -rf /tmp/mds
