ecc39396ed1c00660002c3a6009965e366c7c297
fv3rdugo <fv3rdugo@gmail.com>
2017-05-02 18:17:38 +0200

    Fix title from filename

--- templer/templerfy.sh
+++ templer/templerfy.sh
@@ -14,13 +14,13 @@ git checkout gh-pages && echo "Checkout gh-pages..."|| echo "Checkout gh-pages f
 mkdir -p templer/input/docs templer/input/devel-docs
 rm documentation/docs/* documentation/devel-docs/*
 for i in `ls /tmp/mds/docs/*.md`;do
-    NAME=(basename $i .md)
+    NAME=`basename $i .md`
     echo -e "title: ${NAME//_/ }\n----\n$(cat $i)" > $i
     eval cp $i ~$PATH1/templer/input/docs/
 done
 
 for i in `ls /tmp/mds/devel-docs/*.md`;do
-    NAME=(basename $i .md)
+    NAME=`basename $i .md`
     echo -e "title: ${NAME//_/ }\n----\n$(cat $i)" > $i
     eval cp $i ~$PATH1/templer/input/devel-docs/
 done
