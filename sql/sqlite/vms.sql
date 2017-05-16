ae11c8266b8ce0b138650b6504d76aaf51075df9
fv3rdugo <fv3rdugo@gmail.com>
2017-05-16 18:49:50 +0200

    Add connection_args to vms table  # Si us plau, introduïu el missatge de comissió dels vostres canvis.

--- sql/sqlite/vms.sql
+++ sql/sqlite/vms.sql
@@ -1,8 +1,9 @@
 create table vms (
     `id` integer NOT NULL PRIMARY KEY AUTOINCREMENT
 ,    `name` char(64) NOT NULL
-,    `vm_type` char(20) NOT NULL
+,    `vm_type` char(20) NOT NULL DEFAULT 'KVM'
 ,    `hostname` varchar(128) NOT NULL
 ,    `default_storage` varchar(64) DEFAULT 'default'
+,    `connection_args` text DEFAULT NULL
 ,    UNIQUE (`name`)
 );
