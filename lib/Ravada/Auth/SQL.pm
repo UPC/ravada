4c3ff2931737dbb3720b61895b56f26719892ea0
Francesc Guasch <frankie@etsetb.upc.edu>
2017-05-31 12:12:54 +0200

    [#231] Grant and revoke admin permissions

--- lib/Ravada/Auth/SQL.pm
+++ lib/Ravada/Auth/SQL.pm
@@ -266,13 +266,15 @@ Remove user admin privileges. Returns nothing.
 =cut
 
 sub remove_admin($self, $id) {
-    warn "\t remove_admin $id";
     my $sth = $$CON->dbh->prepare(
             "UPDATE users SET is_admin=NULL WHERE id=?");
 
     $sth->execute($id);
     $sth->finish;
 
+    my $user = $self->search_by_id($id);
+    $self->revoke_all_permissions($user);
+    $self->grant_user_permissions($user);
 }
 
 =head2 is_admin
@@ -487,6 +489,25 @@ sub grant_admin_permissions($self,$user) {
     $sth->finish;
 
 }
+
+=head2 revoke_all_permissions
+
+Revoke all permissions from an user
+
+=cut
+
+sub revoke_all_permissions($self,$user) {
+    my $sth = $$CON->dbh->prepare(
+            "SELECT name FROM grant_types "
+    );
+    $sth->execute();
+    while ( my ($name) = $sth->fetchrow) {
+        $self->revoke($user,$name);
+    }
+    $sth->finish;
+
+}
+
 
 =head2 grant
 
