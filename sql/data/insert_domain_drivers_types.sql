af3f22e73b6a5ebf3d646ac8f20180708ebe390b
fv3rdugo <fv3rdugo@gmail.com>
2017-06-02 19:06:05 +0200

    Change graphics tab for video compression

--- sql/data/insert_domain_drivers_types.sql
+++ sql/data/insert_domain_drivers_types.sql
@@ -1,3 +1,4 @@
 INSERT INTO `domain_drivers_types` VALUES (1,'video','Video Card','qemu');
 INSERT INTO `domain_drivers_types` VALUES (2,'sound','Sound Card','qemu');
 INSERT INTO `domain_drivers_types` VALUES (3,'network','Network Card','qemu');
+INSERT INTO `domain_drivers_types` VALUES (4,'graphics','Graphics Options','qemu');
