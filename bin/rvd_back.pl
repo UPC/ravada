0f647f9e70c60f783d30088d5d346642ab44690c
Francesc Guasch <frankie@etsetb.upc.edu>
2017-06-06 17:10:28 +0200

    Show version when launching rvd_back

--- bin/rvd_back.pl
+++ bin/rvd_back.pl
@@ -69,7 +69,7 @@ my $PID_LONGS;
 #
 
 sub do_start {
-    warn "Starting rvd_back v".$Ravada::VERSION."\n";
+    warn "Starting rvd_back v".Ravada::version."\n";
     my $old_error = ($@ or '');
     my $cnt_error = 0;
 
