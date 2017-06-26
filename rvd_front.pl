50d05288602adf5784380bffd6357ada69f4e91a
Francesc Guasch <frankie@etsetb.upc.edu>
2017-06-26 12:16:57 +0200

    [#231] can admin if is operator

--- rvd_front.pl
+++ rvd_front.pl
@@ -200,6 +200,9 @@ any '/admin' => sub {
 any '/admin/(#type)' => sub {
   my $c = shift;
 
+  return admin($c)  if $c->stash('type') eq 'machines'
+                        && $USER->is_operator;
+
   return access_denied($c)    if !$USER->is_admin;
   return admin($c);
 };
