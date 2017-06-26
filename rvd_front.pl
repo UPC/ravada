b3e0872d5a5cf78cb3eb53d06062dd966875c783
Francesc Guasch <frankie@etsetb.upc.edu>
2017-06-26 15:39:36 +0200

    Hide this web from robots

--- rvd_front.pl
+++ rvd_front.pl
@@ -121,7 +121,7 @@ hook before_routes => sub {
 
   return login($c)
     if
-        $url !~ m{^/(anonymous|login|logout|requirements)}
+        $url !~ m{^/(anonymous|login|logout|requirements|robots.txt)}
         && $url !~ m{^/(css|font|img|js)}
         && !_logged_in($c);
 
@@ -130,6 +130,12 @@ hook before_routes => sub {
 
 
 ############################################################################3
+
+any '/robots.txt' => sub {
+    my $c = shift;
+    warn "robots";
+    return $c->render(text => "User-agent: *\nDisallow: /\n", format => 'text');
+};
 
 any '/' => sub {
     my $c = shift;
@@ -932,6 +938,7 @@ sub req_new_domain {
     my $c = shift;
     my $name = $c->param('name');
     my $swap = ($c->param('swap') or 0);
+    my $vm = ( $c->param('backend') or 'KVM');
     $swap *= 1024*1024*1024;
     my %args = (
            name => $name
