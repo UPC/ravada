f681e844a738b32527dd5b395a7e3f2484e6d5a7
Francesc Guasch <frankie@telecos.upc.edu>
2017-08-09 18:36:36 +0200

    Create permission if not defined

--- lib/Ravada/Auth/SQL.pm
+++ lib/Ravada/Auth/SQL.pm
@@ -592,7 +592,7 @@ sub grant($self,$user,$permission,$value=1) {
     return $value if defined $value_sql && $value_sql eq $value;
 
     my $id_grant = _search_id_grant($permission);
-    if (! defined $value_sql ) {
+    if (! defined $user->can_do($permission)) {
         my $sth = $$CON->dbh->prepare(
             "INSERT INTO grants_user "
             ." (id_grant, id_user, allowed)"
