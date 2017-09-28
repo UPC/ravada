5b38d44caa95e8c611cebb3a32629917e14f0014
Francesc Guasch <frankie@telecos.upc.edu>
2017-09-28 15:42:46 +0200

    [#356] shutdown by id domain

--- lib/Ravada/Request.pm
+++ lib/Ravada/Request.pm
@@ -52,7 +52,7 @@ our %VALID_ARG = (
      ,pause_domain => $args_manage
     ,resume_domain => {%$args_manage, remote_ip => 1 }
     ,remove_domain => $args_manage
-    ,shutdown_domain => { name => 1, uid => 1, timeout => 2, at => 2 }
+    ,shutdown_domain => { name => 2, id_domain => 2, uid => 1, timeout => 2, at => 2 }
     ,force_shutdown_domain => { name => 1, uid => 1, at => 2 }
     ,screenshot_domain => { id_domain => 1, filename => 2 }
     ,start_domain => {%$args_manage, remote_ip => 1 }
@@ -300,6 +300,9 @@ sub shutdown_domain {
     my $args = _check_args('shutdown_domain', @_ );
 
     $args->{timeout} = 10 if !exists $args->{timeout};
+
+    confess "ERROR: You must supply either id_domain or name ".Dumper($args)
+        if !$args->{id_domain} && !$args->{name};
 
     my $self = {};
     bless($self,$class);
