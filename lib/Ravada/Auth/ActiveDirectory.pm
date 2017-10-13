a1cab5a19984f754fabb869085adfaa6c87acc21
Francesc Guasch <frankie@etsetb.upc.edu>
2017-10-13 15:15:53 +0200

    [#392] AD fields and login method

--- lib/Ravada/Auth/ActiveDirectory.pm
+++ lib/Ravada/Auth/ActiveDirectory.pm
@@ -13,12 +13,47 @@ use Carp qw(carp);
 use Data::Dumper;
 use Digest::SHA qw(sha1_hex);
 use Moose;
+use Auth::ActiveDirectory;
 
 use Ravada::Auth::SQL;
 
 with 'Ravada::Auth::User';
 
 our $CONFIG = \$Ravada::CONFIG;
+
+######################################################3
+
+has host => (
+           is => 'ro'
+         ,isa => 'Str'
+    ,required =>1
+);
+
+has port => (
+           is => 'ro'
+         ,isa => 'Str'
+    ,default => 389
+);
+
+has timeout => (
+            is => 'ro'
+         ,isa => 'Str'
+    ,default => 60
+);
+
+has domain => (
+           is => 'ro'
+         ,isa => 'Str'
+    ,required => 1
+);
+
+has principal => (
+           is => 'ro'
+         ,isa => 'Str'
+    ,required => 1
+);
+
+#########################################################
 
 sub add_user {
     die "TODO";
@@ -30,6 +65,28 @@ sub is_admin {
 
 sub is_external {
     return 1;
+}
+
+sub BUILD {
+    my $self = shift;
+
+    die "ERROR: Login failed ".$self->name  if !$self->login;
+}
+
+sub login {
+    my $self = shift;
+
+    my $ad = Auth::ActiveDirectory->new(
+       host      => $self->host,
+       port      => $self->port || 389,
+       timeout   => $self->timeout || 60,
+       domain    => $self->domain,
+       principal => $self->principal,
+    );
+
+    die $ad->error_message if $ad->error_message;
+
+    return $ad->authenticate($self->name, $self->password);
 }
 
 1;
