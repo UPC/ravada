8db378007c2f7bf4feed704a529ab4bec648c2b8
Francesc Guasch <frankie@etsetb.upc.edu>
2017-10-24 16:16:16 +0200

    [#202] Get type from object type properly

--- lib/Ravada/Domain.pm
+++ lib/Ravada/Domain.pm
@@ -1692,7 +1692,7 @@ Returns the virtual machine type as a string.
 
 sub type {
     my $self = shift;
-    my ($type) = $self =~ m{.*::(.*)};
+    my ($type) = ref $self =~ m{.*::(.*)};
     return $type;
 }
 
