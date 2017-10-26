71ad730a59d048ad9fb79cabc7fd5c3265788d78
Francesc Guasch <frankie@etsetb.upc.edu>
2017-10-26 15:46:25 +0200

    [#202] fetch the current domain

--- public/js/ravada.js
+++ public/js/ravada.js
@@ -139,7 +139,8 @@
                   if (!$scope.new_name) {
                     $scope.new_name =   $scope.showmachine.name;
                   }
-                return;
+                  $scope.domain = response.data[i];
+                  return;
                 }
               }
               window.location.href = "/admin/machines";
@@ -194,16 +195,16 @@
           $scope.toggle_base= function(vmId,machineId) {
             $http.get("/machine/toggle_base_vm/" +vmId+ "/" +machineId+".json")
               .then(function(response) {
-                  $scope.message=response.data.message;
+                    $scope.getReqs();
               });
           };
           //On load code
           $scope.showmachineId = window.location.pathname.split("/")[3].split(".")[0] || -1 ;
-          $scope.getSingleMachine();
           // $scope.updatePromise = $interval($scope.getSingleMachine,3000);
           $scope.getReqs= function() {
             $http.get('/requests.json').then(function(response) {
                 $scope.requests=response.data;
+                $scope.getSingleMachine();
             });
           };
           $interval($scope.getReqs,5000);
