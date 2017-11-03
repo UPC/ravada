8eb86709cd68e7a9595409f57e80adb4d5cb0f3e
Francesc Guasch <frankie@etsetb.upc.edu>
2017-11-03 12:23:06 +0100

    [#443] get domain info

--- lib/Ravada/Front.pm
+++ lib/Ravada/Front.pm
@@ -186,7 +186,7 @@ sub list_domains {
     my $self = shift;
     my %args = @_;
 
-    my $query = "SELECT name, id, id_base, is_base FROM domains ORDER BY name";
+    my $query = "SELECT name, id, id_base, is_base FROM domains ";
 
     my $where = '';
     for my $field ( sort keys %args ) {
@@ -195,7 +195,7 @@ sub list_domains {
     }
     $where = "WHERE $where" if $where;
 
-    my $sth = $CONNECTOR->dbh->prepare("$query $where");
+    my $sth = $CONNECTOR->dbh->prepare("$query $where ORDER BY name");
     $sth->execute(map { $args{$_} } sort keys %args);
     
     my @domains = ();
