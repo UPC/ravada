a99ca7c0db93cccc8154037cd8922347a21ea71f
Francesc Guasch <frankie@etsetb.upc.edu>
2017-11-27 12:24:39 +0100

    [#441] allow prepare base if only requuest is the current one

--- lib/Ravada/VM.pm
+++ lib/Ravada/VM.pm
@@ -306,15 +306,25 @@ sub _check_require_base {
     my $self = shift;
 
     my %args = @_;
-    return if !$args{id_base};
-    
-    my $base = Ravada::Domain->open($args{id_base});
-    if ($base->list_requests) {
-        die "ERROR: Domain ".$base->name." has ".$base->list_requests
-            ." requests.\n";
+
+    my $id_base = delete $args{id_base} or return;
+    my $request = delete $args{request};
+    my $id_owner = delete $args{id_owner}
+        or confess "ERROR: id_owner required ";
+
+    delete @args{'_vm','name','vm', 'memory','description'};
+
+    confess "ERROR: Unknown arguments ".join(",",keys %args)
+        if keys %args;
+
+    my $base = Ravada::Domain->open($id_base);
+    if (my @requests = $base->list_requests) {
+        confess "ERROR: Domain ".$base->name." has ".$base->list_requests
+                            ." requests.\n"
+            unless scalar @requests == 1 && $request
+                && $requests[0]->id eq $request->id;
     }
 
-    my $id_owner = $args{id_owner} or confess "ERROR: id_owner required ";
 
     die "ERROR: Domain ".$self->name." is not base"
             if !$base->is_base();
