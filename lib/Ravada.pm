c7cdac65dcf36bf259e1489286eff6f8df77fd0a
Francesc Guasch <frankie@telecos.upc.edu>
2018-01-08 11:00:36 +0100

    [#202] cache active status on db

--- lib/Ravada.pm
+++ lib/Ravada.pm
@@ -738,6 +738,7 @@ sub _upgrade_tables {
     $self->_upgrade_table('domains','description','text DEFAULT NULL');
     $self->_upgrade_table('domains','run_timeout','int DEFAULT NULL');
     $self->_upgrade_table('domains','id_vm','int DEFAULT NULL');
+    $self->_upgrade_table('domains','is_active','int DEFAULT NULL');
 
     $self->_upgrade_table('vms','security','varchar(255) default NULL');
 }
