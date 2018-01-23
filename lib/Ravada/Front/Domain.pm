470a2deaba0cb207eb0f116fe2053421d77c0422
Francesc Guasch <frankie@etsetb.upc.edu>
2018-01-23 10:45:14 +0100

    [#518] load and basic features work

--- lib/Ravada/Front/Domain.pm
+++ lib/Ravada/Front/Domain.pm
@@ -17,27 +17,68 @@ use feature qw(signatures);
 
 with 'Ravada::Domain';
 
+###########################################################################
+#
+our $CONNECTOR = \$Ravada::Front::CONNECTOR;
+#
+###########################################################################
+
+sub BUILD($self, $arg) {
+    my $id = $arg->{id} or confess "ERROR: id required";
+    return $self->_select_domain_db( id => $id);
+}
+
 sub _do_force_shutdown  { confess "TODO" }
 sub add_volume          { confess "TODO" }
 sub clean_swap_volumes  { confess "TODO" }
 sub disk_device         { confess "TODO" }
 sub disk_size           { confess "TODO" }
-sub display             { confess "TODO" }
+
+sub display($self, $user) {
+    return $self->_data('display');
+}
+
 sub force_shutdown      { confess "TODO" }
 sub get_info            { confess "TODO" }
 sub hybernate           { confess "TODO" }
-sub is_active           { confess "TODO" }
-sub is_hibernated       { confess "TODO" }
-sub is_paused           { confess "TODO" }
+
+sub is_active($self) {
+    return 1 if $self->_data('status') eq 'active';
+    return 0;
+}
+
+sub is_hibernated($self) {
+    return 1 if $self->_data('status') eq 'hibernated';
+    return 0;
+}
+
+sub is_paused($self) {
+    return 1 if $self->_data('status') eq 'paused';
+    return 0;
+}
+
 sub is_removed          { confess "TODO" }
 sub list_volumes        { confess "TODO" }
-sub name                { confess "TODO" }
+
+sub name($self) {
+    return $self->_data('name') 
+}
+
 sub pause               { confess "TODO" }
 sub prepare_base        { confess "TODO" }
 sub remove              { confess "TODO" }
 sub rename              { confess "TODO" }
 sub resume              { confess "TODO" }
 sub screenshot          { confess "TODO" }
+
+sub search_domain($self,$name) {
+    my $sth = $$CONNECTOR->dbh->prepare("SELECT id FROM domains WHERE name=?");
+    $sth->execute($name);
+    my ($id) = $sth->fetchrow;
+    $sth->finish;
+    return Ravada::Front::Domain->new(id => $id);
+}
+
 sub set_max_mem         { confess "TODO" }
 sub set_memory          { confess "TODO" }
 sub shutdown            { confess "TODO" }
