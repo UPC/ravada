b7b1d25114974dfa011b312d98289f3b491016fb
Francesc Guasch <frankie@etsetb.upc.edu>
2018-01-23 09:38:11 +0100

    [#518] cache display status

--- lib/Ravada/Domain.pm
+++ lib/Ravada/Domain.pm
@@ -128,7 +128,7 @@ has 'description' => (
 # Method Modifiers
 #
 
-before 'display' => \&_allowed;
+around 'display' => \&_around_display;
 
 around 'add_volume' => \&_around_add_volume;
 
@@ -432,6 +432,13 @@ sub _allowed {
 
     confess $err if $err;
 
+}
+
+sub _around_display($orig,$self,$user) {
+    $self->_allowed($user);
+    my $display = $self->$orig($user);
+    $self->_data(display => $display);
+    return $display;
 }
 ##################################################################################3
 
