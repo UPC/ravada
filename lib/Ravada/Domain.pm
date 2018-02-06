93c2dd82a5eb8ece975c2bd0b87dccd96b6e1d57
Francesc Guasch <frankie@etsetb.upc.edu>
2018-02-06 09:22:14 +0100

    [#533] only remove on active domains

--- lib/Ravada/Domain.pm
+++ lib/Ravada/Domain.pm
@@ -927,7 +927,7 @@ sub _after_remove_domain {
 # removes domain in other VMs
 sub _remove_domain_cascade($self,$user, $cascade = 1) {
 
-    my $sth = $$CONNECTOR->dbh->prepare("SELECT id,name FROM vms");
+    my $sth = $$CONNECTOR->dbh->prepare("SELECT id,name FROM vms WHERE is_active=1");
     my ($id, $name);
     $sth->execute();
     $sth->bind_columns(\($id, $name));
