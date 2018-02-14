45cfddfb9fd7f9ea0b4f71ab786c6b67e4762d1f
Francesc Guasch <frankie@etsetb.upc.edu>
2018-02-14 17:05:04 +0100

    [#547] reconnect closed qemus

--- lib/Ravada/VM/KVM.pm
+++ lib/Ravada/VM/KVM.pm
@@ -1906,6 +1906,13 @@ sub ping($self) {
     eval { $self->vm->list_defined_networks };
     warn $@ if $@;
     return 1 if !$@;
+    if ($@ =~ /libvirt error code: 1,/) {
+        $self->disconnect();
+        $self->connect;
+        eval { $self->vm->list_defined_networks };
+        return 1 if !$@;
+    }
+    warn $@ if $@;
     return 0;
 }
 
