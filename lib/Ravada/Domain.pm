3f02538849fe56b4806cac57cbf5a570043d23d3
Francesc Guasch <frankie@etsetb.upc.edu>
2018-02-15 12:05:32 +0100

    [#547] in some rare cases port is missing spit it out
    So we can find out where it comes from. My guess
    the domain was down.

--- lib/Ravada/Domain.pm
+++ lib/Ravada/Domain.pm
@@ -1562,6 +1562,7 @@ sub _delete_ip_rule_remote($self, $iptables, $vm = $self->_vm) {
 }
 sub _open_port($self, $user, $remote_ip, $local_ip, $local_port, $jump = 'ACCEPT') {
     $self->_vm->create_iptables_chain($IPTABLES_CHAIN);
+    confess "local port undefined " if !$local_port;
     my @iptables_arg = ($remote_ip
                         ,$local_ip, 'filter', $IPTABLES_CHAIN, $jump,
                         ,{'protocol' => 'tcp', 's_port' => 0, 'd_port' => $local_port});
