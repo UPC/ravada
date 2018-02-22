611e0361ef98cccf1a7baf4e81f41a8a635133a9
Francesc Guasch <frankie@etsetb.upc.edu>
2018-02-22 16:01:46 +0100

    [#447] [test] iso images definition

--- sql/sqlite/iso_images.sql
+++ sql/sqlite/iso_images.sql
@@ -9,6 +9,8 @@ CREATE TABLE `iso_images` (
 ,  `url` varchar(255)
 ,  `md5` varchar(32)
 ,  `md5_url` varchar(255)
+,  `sha256_url` varchar(255)
 ,  `device` varchar(255)
+,  `min_disk_size` integer
 ,  UNIQUE (`name`)
 );
