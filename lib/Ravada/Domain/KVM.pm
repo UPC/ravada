e81530e1d4ab3faac4b4615f43370fbaa6be9fdc
Francesc Guasch <frankie@ebdsoft.com>
2018-03-05 16:13:43 +0100

    [#589] set autostart on KVM

--- lib/Ravada/Domain/KVM.pm
+++ lib/Ravada/Domain/KVM.pm
@@ -1578,4 +1578,9 @@ sub internal_id($self) {
     return $self->domain->get_id();
 }
 
+sub autostart($self, $value=undef) {
+    $self->domain->set_autostart($value) if defined $value;
+    return $self->domain->get_autostart();
+}
+
 1;
