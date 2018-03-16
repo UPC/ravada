92ab065601c19578fc42b7218f80affc73e8a883
Francesc Guasch <frankie@etsetb.upc.edu>
2018-03-16 17:39:45 +0100

    [#231] has role operator if it can prepare bases

--- lib/Ravada/Auth/SQL.pm
+++ lib/Ravada/Auth/SQL.pm
@@ -347,7 +347,7 @@ sub is_operator {
         || $self->can_shutdown_clone()
 	|| $self->can_hibernate_clone
 	|| $self->can_change_settings_clones()
-        || $self->can_remove_clone();
+        || $self->can_remove_clone()
         || $self->can_create_base();
 }
 
