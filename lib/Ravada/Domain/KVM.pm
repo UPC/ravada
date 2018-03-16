67ea8882e2b6cf87177ecde39b2d5a8f5e46901e
Francesc Guasch <frankie@etsetb.upc.edu>
2018-03-16 16:11:11 +0100

    [#589] fixed autostart for readonly frontend domains

--- lib/Ravada/Domain/KVM.pm
+++ lib/Ravada/Domain/KVM.pm
@@ -1618,7 +1618,7 @@ sub internal_id($self) {
     return $self->domain->get_id();
 }
 
-sub autostart($self, $value=undef) {
+sub autostart($self, $value=undef, $user=undef) {
     $self->domain->set_autostart($value) if defined $value;
     return $self->domain->get_autostart();
 }
