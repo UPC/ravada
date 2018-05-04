83009b531d7e8db55cb72d95eee9f85138b72893
Francesc Guasch <frankie@etsetb.upc.edu>
2018-05-04 18:10:05 +0200

    fixed write access to domain from front

--- rvd_front.pl
+++ rvd_front.pl
@@ -1180,7 +1180,7 @@ sub show_link {
     my ($display_ip, $display_port) = $uri =~ m{\w+://(\d+\.\d+\.\d+\.\d+):(\d+)};
     my $description = $domain->description;
     if (!$description && $domain->id_base) {
-        my $base = Ravada::Domain->open($domain->id_base);
+        my $base = $RAVADA->search_domain_by_id($domain->id_base);
         $description = $base->description();
     }
     $c->stash(description => $description);
