422f1e5e5610946393efcd2047b8b6a96a95cbb2
JanFontanet <jan2_@hotmail.es>
2018-06-01 12:10:20 +0200

    [733] Added usb's on create machine

--- lib/Ravada/VM/KVM.pm
+++ lib/Ravada/VM/KVM.pm
@@ -1519,6 +1519,7 @@ sub _xml_add_usb_xhci {
     $address->setAttribute(bus => '0x00');
     $address->setAttribute(slot => '0x07');
     $address->setAttribute(function => '0x0');
+    
 }
 
 sub _xml_add_usb_ehci1 {
