b50c4ea99825c8ce1635cd60f6e81b2647a2232d
Francesc Guasch <frankie@etsetb.upc.edu>
2018-06-05 15:22:22 +0200

    fix(backend): don't download custom ISOs with no url
    issue #780

--- lib/Ravada/VM/KVM.pm
+++ lib/Ravada/VM/KVM.pm
@@ -604,7 +604,7 @@ sub _domain_create_from_iso {
     my $device_cdrom;
 
     confess "Template ".$iso->{name}." has no URL, iso_file argument required."
-        if !$iso->{url} && !$args{iso_file};
+        if !$iso->{url} && !$args{iso_file} && !$iso->{device};
 
     if (exists $args{iso_file} && !($args{iso_file} eq "<NONE>")){
       $device_cdrom = $args{iso_file};
@@ -874,7 +874,7 @@ sub _iso_name {
     my $device = ($iso->{device} or $self->dir_img."/$iso_name");
 
     confess "Missing MD5 and SHA256 field on table iso_images FOR $iso->{url}"
-        if !$iso->{md5} && !$iso->{sha256};
+        if $iso->{url} && !$iso->{md5} && !$iso->{sha256};
 
     my $downloaded = 0;
     if (! -e $device || ! -s $device) {
@@ -1085,6 +1085,11 @@ sub _fetch_filename {
     my $self = shift;
     my $row = shift;
 
+    if (!$row->{file_re} && !$row->{url} && $row->{device}) {
+         my ($file) = $row->{device} =~ m{.*/(.*)};
+         $row->{filename} = $file;
+         return;
+    }
     return if !$row->{file_re} && !$row->{url} && !$row->{device};
     if (!$row->{file_re}) {
         my ($new_url, $file);
