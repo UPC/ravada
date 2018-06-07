bbe2e326701f94e1948f79789fcbb279ecd5bb1b
Francesc Guasch <frankie@etsetb.upc.edu>
2018-06-07 17:45:37 +0200

    wip(frontend): allow manage machine
    issue #698

--- lib/Ravada/Front.pm
+++ lib/Ravada/Front.pm
@@ -186,7 +186,6 @@ sub list_machines($self, $user) {
         }
         push @list,(@$machines);
     }
-
     push @list,(@{$self->list_clones()}) if $user->can_list_clones;
     if ($user->can_create_base || $user->can_create_machine || $user->is_operator) {
         my $machines = $self->list_domains(id_owner => $user->id);
