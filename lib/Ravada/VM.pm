93616c3dfa3966ed02cb5874381bc27ffca68437
Francesc Guasch <frankie@telecos.upc.edu>
2018-08-09 18:34:36 +0200

    refactor(backend): check create domain args
    issue #838

--- lib/Ravada/VM.pm
+++ lib/Ravada/VM.pm
@@ -295,7 +295,20 @@ sub _around_create_domain {
     my $owner = Ravada::Auth::SQL->search_by_id($id_owner);
 
     my $base;
+    my $remote_ip = delete $args{remote_ip};
     my $id_base = delete $args{id_base};
+     my $id_iso = delete $args{id_iso};
+     my $active = delete $args{active};
+       my $name = delete $args{name};
+       my $swap = delete $args{swap};
+
+     delete $args{memory};
+     delete $args{request};
+     delete $args{iso_file};
+     delete @args{'description','remove_cpu','vm'};
+
+    confess "ERROR: Unknown args ".Dumper(\%args) if keys %args;
+
     $base = Ravada::Domain->open($id_base)  if $id_base;
 
     confess "ERROR: User ".$owner->name." is not allowed to create machines"
@@ -309,8 +322,7 @@ sub _around_create_domain {
     $self->_pre_create_domain(@_);
 
     my $domain = $self->$orig(@_);
-
-    $domain->add_volume_swap( size => $args{swap})  if $args{swap};
+    $domain->add_volume_swap( size => $swap )   if $swap;
 
     if ($id_base) {
         $domain->run_timeout($base->run_timeout)
@@ -320,12 +332,11 @@ sub _around_create_domain {
     $domain->is_volatile(1)     if $user->is_temporary() ||($base && $base->volatile_clones());
 
     my @start_args = ( user => $owner );
-    my $remote_ip = $args{remote_ip};
     push @start_args, (remote_ip => $remote_ip) if $remote_ip;
 
     $domain->_post_start(@start_args) if $domain->is_active;
     eval {
-           $domain->start(@start_args)      if $domain->is_volatile && ! $domain->is_active;
+           $domain->start(@start_args)      if $active || ($domain->is_volatile && ! $domain->is_active);
     };
     die $@ if $@ && $@ !~ /code: 55,/;
 
@@ -452,7 +463,7 @@ sub ip {
     return $ip if $ip && $ip !~ /^127/ && $ip =~ /^\d+\.\d+\.\d+\.\d+$/;
 
     warn "WARNING: I can't find the IP of host ".$self->host.", using localhost."
-        ." This virtual machine won't be available from the network.";
+        ." This virtual machine won't be available from the network." if $0 !~ /\.t$/;
 
     return '127.0.0.1';
 }
