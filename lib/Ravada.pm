caf7890a7896192d5cbc3f13f624bfa1040175dc
Daniel Sanchez <dani@fib.upc.edu>
2018-11-15 14:42:11 +0100

    Feature/918 pxe (#938)
    * wip(frontend): ISO file is no longer mandatory
    
    issue #918
    
    * Added templete for empty 32bit machine
    
    issue #918
    
    *     Added template for empty 64bit machine
    
        issue #918
    
    * Modify empty i386 vm
    Added empty x64 vm in definition list
    
    issue #918
    
    * Fixed white spaces
    
    * Fixed white spaces
    
    * Change comments format to standarize formats
    Now, descriptions are like "Machine 32 bits" w
    
    issue #918

--- lib/Ravada.pm
+++ lib/Ravada.pm
@@ -501,6 +501,20 @@ sub _update_isos {
           ,xml_volume => 'wisuvolume.xml'
           ,min_disk_size => '21'
         }
+       ,empty_32bits => {
+          name => 'Empty Machine 32 bits'
+          ,description => 'Empty Machine 32 bits Boot PXE'
+          ,xml => 'empty-i386.xml'
+          ,xml_volume => 'jessie-volume.xml'
+          ,min_disk_size => '0'
+        }
+       ,empty_64bits => {
+          name => 'Empty Machine 64 bits'
+          ,description => 'Empty Machine 64 bits Boot PXE'
+          ,xml => 'empty-amd64.xml'
+          ,xml_volume => 'jessie-volume.xml'
+          ,min_disk_size => '0'
+        }
     );
 
     $self->_update_table($table, $field, \%data);
