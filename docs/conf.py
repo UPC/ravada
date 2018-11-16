45eb8f4e1ef0608f1821b99c8a40b50f3e97628f
Fernando Verdugo <fv3rdugo@gmail.com>
2018-11-16 15:02:14 +0100

    Add static path

--- docs/conf.py
+++ docs/conf.py
@@ -26,13 +26,9 @@ html_search_language = 'en'
 
 # Theme options
 html_theme_options = {
-    # 'typekit_id': 'hiw1hhg',
-    # 'analytics_id': '',
-    # 'sticky_navigation': True  # Set to False to disable the sticky nav while scrolling.
     'logo_only': True,  # if we have a html_logo below, this shows /only/ the logo with no title text
     'collapse_navigation': False,  # Collapse navigation (False makes it tree-like)
-    # 'display_version': True,  # Display the docs version
-    # 'navigation_depth': 4,  # Depth of the headers shown in the navigation bar
 }
+html_static_path = []
 
 html_logo = '/img/logo_ravada.png'
