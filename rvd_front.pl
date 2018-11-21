5ae82eca29c97de840a764b21f704d844c8d5bf9
Francesc Guasch <frankie@etsetb.upc.edu>
2018-11-21 16:13:46 +0100

    feature(frontend): shutdown machines on disabling node
    issue #947

--- rvd_front.pl
+++ rvd_front.pl
@@ -281,6 +281,12 @@ get '/node/enable/(:id).json' => sub {
 get '/node/disable/(:id).json' => sub {
     my $c = shift;
     return access_denied($c) if !$USER->is_admin;
+
+    my $machines = $RAVADA->_list_machines_vm($c->stash('id'));
+    for ( @$machines ) {
+        my $req = Ravada::Request->shutdown_domain( uid => $USER->id , id_domain => $_->{id} );
+        warn "$_->{id} ".$req->id;
+    }
     return $c->render(json => {enabled => $RAVADA->enable_node($c->stash('id'),0)});
 };
 get '/node/remove/(:id).json' => sub {
