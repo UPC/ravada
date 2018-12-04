8e58f544eb974c77e0e6aa97d31ce26d97e3db13
Francesc Guasch <frankie@etsetb.upc.edu>
2018-12-04 16:27:02 +0100

    feature(repository): verbose output
    issue #959

--- lib/Ravada/Repository/ISO.pm
+++ lib/Ravada/Repository/ISO.pm
@@ -14,19 +14,28 @@ $DIR_ISO_YML = "/var/lib/ravada/repository/iso" if $0 =~ m{^/usr/sbin};
 
 our $CONNECTOR = \$Ravada::CONNECTOR;
 
-sub insert_iso_yml($file) {
+sub insert_iso_yml($file, $verbose = 0) {
     my $entry = LoadFile($file);
-    Ravada::_update_table(undef, 'iso_images','name', { $file => $entry } );
+    return Ravada::_update_table(undef, 'iso_images','name', { $file => $entry }, $verbose );
 }
 
-sub insert_iso_locale($locale) {
+sub insert_iso_locale($locale, $verbose = 0) {
+
+    my $n_found = 0;
+
     my $dir = "$DIR_ISO_YML/$locale";
-    opendir my $ls,$dir or return;
+    opendir my $ls,$dir or do {
+        return 0;
+    };
     while (my $file = readdir $ls) {
         next if $file !~ /\.yml$/;
-        insert_iso_yml("$dir/$file");
+        insert_iso_yml("$dir/$file", $verbose);
+
+        $n_found++;
     }
     closedir $ls;
+
+    return $n_found;
 }
 
 1;
