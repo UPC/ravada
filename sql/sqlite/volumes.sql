339cb665b7702f7781908ef388bca688ae39f198
Francesc Guasch <frankie@etsetb.upc.edu>
2019-01-14 13:07:46 +0100

    wip(volumes): store order number of volume
    So we can sort them for change or removal
    
    issue #973

--- sql/sqlite/volumes.sql
+++ sql/sqlite/volumes.sql
@@ -3,6 +3,8 @@ create table volumes (
 ,    `id_domain` integer NOT NULL
 ,    `name` char(64) NOT NULL
 ,    `file` varchar(255) NOT NULL
+,    `n_order` integer NOT NULL
 ,    `info` TEXT
 ,    UNIQUE (`id_domain`,`name`)
+,    UNIQUE (`id_domain`,`n_order`)
 );
