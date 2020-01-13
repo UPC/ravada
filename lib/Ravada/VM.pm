ca33df5e714eaad5eccbb8bb31e07a5c73efcb8a
Francesc Guasch <frankie@etsetb.upc.edu>
2020-01-13 17:07:17 +0100

    refactor(nodes): do not balance when only 1 node

--- lib/Ravada/VM.pm
+++ lib/Ravada/VM.pm
@@ -1373,7 +1373,7 @@ sub balance_vm($self, $base=undef) {
         @vms = $self->list_nodes();
     }
 #    warn Dumper([ map { $_->name } @vms]);
-    return $self if !@vms;
+    return $self if scalar(@vms)<2;
     for my $vm (_random_list( @vms )) {
         next if !$vm->enabled();
         my $active = 0;
