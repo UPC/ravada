c4f542e047dec331c7ab837faa800eb4db15f796
Francesc Guasch <frankie@etsetb.upc.edu>
2020-01-13 12:59:21 +0100

    feature(volumes): rebase volume files

--- lib/Ravada/Volume/Void.pm
+++ lib/Ravada/Volume/Void.pm
@@ -19,6 +19,14 @@ sub prepare_base($self) {
     $data->{is_base} = 1;
     $data->{origin} = $self->file;
 
+    my $backing_file = $data->{backing_file};
+    if ($backing_file) {
+        my $data2 = Load($self->vm->read_file($self->file));
+        for (keys %$data2) {
+            $data->{$_} = $data2->{$_} if !exists $data->{$_};
+        }
+    }
+
     $self->vm->write_file($base_file, Dump($data));
 
     return $base_file;
@@ -36,13 +44,23 @@ sub _load($self) {
     return Load($self->vm->read_file($self->file));
 }
 
+sub _save($self, $data) {
+    $self->vm->write_file($self->file, Dump($data));
+}
+
 sub clone($self, $clone_file) {
     confess "Error: volume is not a base" if !$self->is_base;
     my $data = {
         backing_file => $self->file
         ,capacity => $self->capacity
     };
+    my $data2 = Load($self->vm->read_file($self->file));
+    for (keys %$data2) {
+        next if /^(origin|capacity|is_base)$/;
+        $data->{$_} = $data2->{$_} if !exists $data->{$_};
+    }
 
+    confess if $clone_file =~ /\.iso$/;
     $self->vm->write_file($clone_file, Dump($data));
     return $clone_file;
 }
@@ -50,7 +68,13 @@ sub clone($self, $clone_file) {
 sub backing_file($self) {
     my $data = $self->_load();
     my $backing_file = $data->{backing_file}
-        or confess "Error: I don't know backing file from ".Dumper($data);
+        or confess "Error: No backing file from ".Dumper($data);
+}
+
+sub rebase($self, $file) {
+    my $data = $self->_load();
+    $data->{backing_file} = $file;
+    $self->_save($data);
 }
 
 1;
