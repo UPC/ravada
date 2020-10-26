ea78982fd4e216682648e50eee739a6882ab775f
robertperez-upc <72500868+robertperez-upc@users.noreply.github.com>
2020-10-26 10:58:07 +0100

    Loading icon (#1401)
    refactor(frontend): Replace loading dots by animated icon

--- public/js/ravada.js
+++ public/js/ravada.js
@@ -830,7 +830,6 @@
 
         };
         $scope.wait_request = function() {
-            $scope.dots += '.';
             if ($scope.id_request) {
                 $http.get('/request/'+$scope.id_request+'.json').then(function(response) {
                     $scope.request=response.data;
@@ -877,7 +876,6 @@
             }
         }
 
-        $scope.dots = '...';
         $scope.redirect_done = false;
         $scope.wait_request();
         $scope.view_clicked=false;
