962ff68fe254a49c7ab2ead26955fa1635d9c55e
frankiejol <frankie@telecos.upc.edu>
2020-11-25 11:21:57 +0100

    wip(pools): auto-number even with only one clone
    issue #1443

--- lib/Ravada.pm
+++ lib/Ravada.pm
@@ -3122,8 +3122,9 @@ sub _cmd_open_iptables {
 
 sub _cmd_clone($self, $request) {
 
-    return _req_clone_many($self, $request) if $request->defined_arg('number')
-    && $request->defined_arg('number') > 1;
+    return _req_clone_many($self, $request)
+        if ( $request->defined_arg('number') && $request->defined_arg('number') > 1)
+            || (! $request->defined_arg('name') && $request->defined_arg('add_to_pool'));
 
     my $domain = Ravada::Domain->open($request->args('id_domain'))
         or confess "Error: Domain ".$request->args('id_domain')." not found";
