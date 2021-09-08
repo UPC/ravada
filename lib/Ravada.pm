bf0efddb7ed74adee3d2f9175429e65b78c8c8c5
Francesc Guasch <frankie@etsetb.upc.edu>
2021-09-08 11:10:39 +0200

    wip: indexes for newest mariadb

--- lib/Ravada.pm
+++ lib/Ravada.pm
@@ -1216,6 +1216,7 @@ sub _add_indexes_generic($self) {
             "unique(id_domain,n_order)"
             ,"unique(id_domain,driver)"
             ,"unique(id_vm,port)"
+            ,"index(id_domain)"
         ]
         ,domain_ports => [
             "unique (id_domain,internal_port):domain_port"
@@ -1224,6 +1225,7 @@ sub _add_indexes_generic($self) {
         ]
         ,group_access => [
             "unique (id_domain,name)"
+            ,"index(id_domain)"
         ]
         ,requests => [
             "index(status,at_time)"
@@ -1254,17 +1256,23 @@ sub _add_indexes_generic($self) {
         ]
         ,booking_entry_ldap_groups => [
             "index(id_booking_entry,ldap_group)"
+            ,"index(id_booking_entry)"
         ]
         ,booking_entry_users => [
             "index(id_booking_entry,id_user)"
+            ,"index(id_booking_entry)"
+            ,"index(id_user)"
         ]
         ,booking_entry_bases => [
             "index(id_booking_entry,id_base)"
+            ,"index(id_base)"
+            ,"index(id_booking_entry)"
         ]
 
         ,volumes => [
-            'UNIQUE (id_domain,name):id_domain',
-            'UNIQUE (id_domain,n_order):id_domain2'
+            "index(id_domain)"
+            ,'UNIQUE (id_domain,name):id_domain_name'
+            ,'UNIQUE (id_domain,n_order):id_domain2'
         ]
 
         ,vms=> [
