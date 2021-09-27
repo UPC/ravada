d94905db3366257c41b650c1209b7630d7bfef9c
Francesc Guasch <frankie@etsetb.upc.edu>
2021-09-27 17:02:25 +0200

    wip(test): start remote node before cleaning

--- t/lib/Test/Ravada.pm
+++ t/lib/Test/Ravada.pm
@@ -1324,6 +1324,7 @@ sub _clean_remote_nodes {
 sub clean_remote_node {
     my $node = shift;
 
+    start_node($node);
     _remove_old_domains_vm($node);
     wait_request(debug => 0);
     _remove_old_disks($node);
