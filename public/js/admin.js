91d2784809161cf913fe5ddb56d27709feb44f19
Francesc Guasch <frankie@etsetb.upc.edu>
2023-01-10 13:04:38 +0100

    wip: frontend list unused volumes

--- public/js/admin.js
+++ public/js/admin.js
@@ -8,8 +8,10 @@ ravadaApp.directive("solShowMachine", swMach)
         .controller("messagesPage", messagesPageC)
         .controller("manage_nodes",manage_nodes)
         .controller("manage_networks",manage_networks)
+        .controller("manage_storage_pools",manage_storage_pools)
         .controller("settings_node",settings_node)
         .controller("settings_network",settings_network)
+        .controller("settings_storage",settings_storage)
         .controller("new_node", newNodeCtrl)
         .controller("settings_global", settings_global_ctrl)
         .controller("admin_groups", admin_groups_ctrl)
@@ -864,6 +866,16 @@ ravadaApp.directive("solShowMachine", swMach)
         list_networks();
     }
 
+
+    function manage_storage_pools($scope, $http, $interval, $timeout) {
+        $scope.list_unused_volumes=function() {
+            $scope.unused_volumes = undefined;
+            $http.get('/storage/list_unused_volumes').then(function(response) {
+                $scope.unused_volumes = response.data;
+            });
+        }
+    }
+
     function newNodeCtrl($scope, $http, $timeout) {
         $http.get('/list_vm_types.json').then(function(response) {
             $scope.backends = response.data;
@@ -1187,6 +1199,12 @@ ravadaApp.directive("solShowMachine", swMach)
         };
         $scope.show_hdev = { 1: true};
 
+    };
+
+    function settings_storage($scope, $http, $timeout) {
+        var url_ws;
+        $scope.init = function() {
+        };
     };
 
     function admin_groups_ctrl($scope, $http) {
