1a4e0e8693c1943aa78d25cf35926cfffdb5c678
Francesc Guasch <frankie@etsetb.upc.edu>
2023-03-23 16:23:24 +0100

    Fix opensuse URL (#1925)
    fix: openSUSE URL

--- lib/Ravada.pm
+++ lib/Ravada.pm
@@ -529,9 +529,8 @@ sub _update_isos {
             ,arch => 'x86_64'
             ,xml => 'bionic-amd64.xml'
             ,xml_volume => 'bionic64-volume.xml'
-            ,url => 'https://download.opensuse.org/distribution/leap/15.0/iso/'
-            ,sha256_url => '$url/openSUSE-Leap-15.\d+-NET-x86_64.iso.sha256'
-            ,file_re => 'openSUSE-Leap-15.\d+-NET-x86_64.iso'
+            ,url => 'https://download.opensuse.org/distribution/leap/15.4/iso/'
+            ,file_re => 'openSUSE-Leap-15.\d-NET-x86_64-Current.iso'
 
         }
         ,xubuntu_beaver_64 => {
