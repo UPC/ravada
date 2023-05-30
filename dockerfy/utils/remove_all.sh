c42cf56ffb700b054a9c59b052d068fb37d50051
Fernando Verdugo <fernando.verdugo@upc.edu>
2023-05-30 11:50:33 +0200

    Fix remove script (#1952) fix: remove images properly
    Issue #1952

--- dockerfy/utils/remove_all.sh
+++ dockerfy/utils/remove_all.sh
@@ -4,8 +4,8 @@ read -r -p "Remove images (front - back - mysql) - Are you sure? [Y/n]" response
 response=${response,,} # tolower
 if [[ $response =~ ^(yes|y| ) ]] || [[ -z $response ]]; then
 	docker image rm mysql:5.7 -f
-	docker image rm ravada/front:latest -f
-	docker image rm ravada/back:latest -f
+	docker image rm dockerfy_ravada-back:latest -f
+	docker image rm dockerfy_ravada-front:latest -f
 fi
 
 read -r -p "Remove volume dockerfy_sshkeys - Are you sure? [Y/n]" response
