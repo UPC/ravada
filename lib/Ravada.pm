4514923e15622f2db00c75eefc421b05202ab6b1
Francesc Guasch <frankie@etsetb.upc.edu>
2023-10-31 08:51:01 +0100

    fix: properly check if process has finished
    Also fixes problem locking machines when preparing

--- lib/Ravada.pm
+++ lib/Ravada.pm
@@ -4499,6 +4499,7 @@ sub _wait_pids($self) {
     for my $type ( keys %{$self->{pids}} ) {
         for my $pid ( keys %{$self->{pids}->{$type}}) {
             my $kid = waitpid($pid , WNOHANG);
+            next if kill(0,$pid);
             push @done, ($pid) if $kid == $pid || $kid == -1;
         }
     }
