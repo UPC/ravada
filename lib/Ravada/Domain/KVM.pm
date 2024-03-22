8310b7312a1f5a31db54ad2e2cfa98699cc18423
Francesc Guasch <frankie@etsetb.upc.edu>
2024-03-22 15:49:43 +0100

    wip: fail request when reload config fails

--- lib/Ravada/Domain/KVM.pm
+++ lib/Ravada/Domain/KVM.pm
@@ -3680,7 +3680,7 @@ sub reload_config($self, $doc) {
         $new_domain = $self->_vm->vm->define_domain($doc->toString);
     };
 
-    cluck ''.$@ if $@;
+    die $@."\n" if$@;
 
     $self->domain($new_domain);
 
