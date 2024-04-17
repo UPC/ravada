e3ee172dff673bdfc0bc8ea8d78ff41bc69d97d0
Francesc Guasch <frankie@etsetb.upc.edu>
2024-04-17 15:05:35 +0200

    Refactor/test volatile (#2039)
    * refactor: short and log tests
    * refactor: properly start nodes
    * refactor: return undef when no virtual manager in localhost

--- lib/Ravada.pm
+++ lib/Ravada.pm
@@ -5684,8 +5684,11 @@ sub _cmd_shutdown_node($self, $request) {
 
 sub _cmd_start_node($self, $request) {
     my $id_node = $request->args('id_node');
-    my $node = Ravada::VM->open($id_node);
-    $node->start();
+    my $node;
+    eval{ $node = Ravada::VM->open($id_node);
+        $node->start() if$node;
+    };
+    Ravada::VM::_wake_on_lan($id_node) if !$node;
 }
 
 sub _cmd_connect_node($self, $request) {
@@ -6505,7 +6508,10 @@ sub search_vm {
     );
     $sth->execute($type, $host);
     my ($id) = $sth->fetchrow();
-    return Ravada::VM->open($id)    if $id;
+    my $vm;
+    $vm = Ravada::VM->open($id)    if $id;
+    return if $host eq 'localhost' && $vm && !$vm->vm;
+
     return if $host ne 'localhost';
 
     my $vms = $self->_create_vm($type);
