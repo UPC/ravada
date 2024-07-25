32aa1ef84d06001e8f94bb57653ade3341a2ef16
Francesc Guasch <frankie@telecos.upc.edu>
2024-07-25 13:22:41 +0200

    wip: openid basic feature

--- lib/Ravada/Auth/OpenID.pm
+++ lib/Ravada/Auth/OpenID.pm
@@ -4,13 +4,12 @@ use strict;
 use warnings;
 
 use Data::Dumper;
-use Authen::ModAuthPubTkt;
-use URI::Escape;
-use LWP::UserAgent;
+
+use Ravada::Front;
 
 =head1 NAME
 
-Ravada::Auth::SSO - SSO library for Ravada
+Ravada::Auth::OpenID - OpenID library for Ravada
 
 =cut
 
@@ -45,11 +44,15 @@ sub _check_user_profile($self) {
         if ($user_sql->external_auth ne 'openid') {
             $user_sql->external_auth('openid');
         }
-        return;
+        return $user_sql;
     }
+
+    return if ! Ravada::Front::setting(undef,'/frontend/auto_create_users');
 
     Ravada::Auth::SQL::add_user(name => $self->name, is_external => 1, is_temporary => 0
         , external_auth => 'openid');
+
+    return $user_sql;
 }
 
 sub is_admin { }
@@ -59,14 +62,14 @@ sub is_external { }
 sub login_external($name, $header) {
 
     for my $field (qw(OIDC_CLAIM_exp OIDC_access_token_expires)) {
-        if ( $header->{$field} < time() ) {
+        if ( exists $header->{$field} && defined $header->{$field} && $header->{$field} < time() ) {
             warn localtime($header->{$field})." $field expired \n";
             return 0;
         }
     }
 
     my $self = Ravada::Auth::OpenID->new(name => $name);
-    $self->_check_user_profile();
+    return if !$self->_check_user_profile();
     return $self;
 }
 
