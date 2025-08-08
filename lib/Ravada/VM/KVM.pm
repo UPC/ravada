4242cdd99ac9d2de9ca1fc2603f7970b01eb2938
Francesc Guasch <frankie@telecos.upc.edu>
2025-08-08 16:07:42 +0200

    wip: create pool when no root

--- lib/Ravada/VM/KVM.pm
+++ lib/Ravada/VM/KVM.pm
@@ -667,8 +667,12 @@ Returns the directory where disk images are stored in this Virtual Manager
 sub dir_img {
     my $self = shift;
 
-    my $pool = $self->_load_storage_pool();
+    my $pool;
+    eval { $pool = $self->_load_storage_pool() };
+    warn $@ if $@;
+
     $pool = $self->_create_default_pool() if !$pool;
+
     my $xml = XML::LibXML->load_xml(string => $pool->get_xml_description());
 
     my $dir = $xml->findnodes('/pool/target/path/text()');
@@ -732,7 +736,7 @@ sub create_storage_pool($self, $name, $dir, $vm=$self->vm) {
         die $@ if $@;
         die "$error\n" if $error;
     }
-
+    return $pool;
 }
 
 sub remove_storage_pool($self, $name) {
@@ -745,12 +749,17 @@ sub remove_storage_pool($self, $name) {
 
 sub _create_default_pool($self, $vm=$self->vm) {
     my $dir = "/var/lib/libvirt/images";
+
+    if ($>) {
+        $dir = "/run/user/$</images";
+    }
     mkdir $dir if ! -e $dir;
 
     my $name = 'default';
 
+    my $pool;
     eval {
-    $self->create_storage_pool($name, $dir, $vm);
+    $pool=$self->create_storage_pool($name, $dir, $vm);
     };
     warn $@ if $@;
 }
